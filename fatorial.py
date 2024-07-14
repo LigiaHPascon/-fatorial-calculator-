aed56e090feebe8c2b76319db038b7c59866323f
LigiaHPascon <pasconligia@gmail.com>
2024-07-14 17:55:00 -0300

    Create fatorial.py

---
+++ fatorial.py
@@ -0,0 +1,13 @@
+def fatorial(n):
+  if n == 0:
+      return 1
+else:
+    return n * fatorial(n -1 )
+if _name_ == "_main_" :
+  num = int(imput("Digite um número inteiro: "))
+                                         print(f"0 fatorial de  {num} é 
+                                    {fatorial (num) }" )     
+  
+
+   
+    
